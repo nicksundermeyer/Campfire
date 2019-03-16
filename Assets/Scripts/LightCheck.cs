19132ceaa5e82ca38496b5e083ab22bd3e9a8476
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2019-03-16 10:52:01 -0600

    First commit

---
+++ Assets/Scripts/LightCheck.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightCheck : MonoBehaviour
+{
+    public RenderTexture lightCheckTexture;
+    public float LightLevel = 1000000000f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // copy rendertexture into temporary rendertexture for this frame
+        RenderTexture tmpTexture = RenderTexture.GetTemporary(lightCheckTexture.width, lightCheckTexture.height, 0, RenderTextureFormat.Default, RenderTextureReadWrite.Linear);
+        Graphics.Blit(lightCheckTexture, tmpTexture);
+        RenderTexture previous = RenderTexture.active;
+        RenderTexture.active = tmpTexture;
+
+        // copy rendertexture values into texture2D
+        Texture2D tmp2DTexture = new Texture2D(lightCheckTexture.width, lightCheckTexture.height);
+        tmp2DTexture.ReadPixels(new Rect(0, 0, tmpTexture.width, tmpTexture.height), 0, 0);
+        tmp2DTexture.Apply();
+
+        // release temporary rendertexture
+        RenderTexture.active = previous;
+        RenderTexture.ReleaseTemporary(tmpTexture);
+
+        // get pixel values out of texture
+        Color32[] colors = tmp2DTexture.GetPixels32();
+        Destroy(tmp2DTexture);
+
+        // calculate light level by adding up all pixel values
+        LightLevel = 0;
+        for(int i=0; i<colors.Length; i++)
+        {
+            // formula to calculate light level from rgb values
+            LightLevel += ((0.2126f * colors[i].r) + (0.7152f * colors[i].g) + (0.0722f * colors[i].b));
+        }
+
+        if(LightLevel == 0f)
+        {
+            LightLevel = 5000000;
+        }
+
+        // Debug.Log("LightLevel: " + LightLevel);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+    //movement variable
+    public float movementSpeed = 20f;
+
+    // light level checking
+    public float minLightValue;
+    private LightCheck lightChecker;
+
+    // time allowed to be in darkness
+    private float darknessTimer = 0f;
+    public float maxDarknessTime;
+
+    private Rigidbody rb;
+
+    private void Awake() {
+        lightChecker = GetComponentInChildren<LightCheck>();
+        rb = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Move();
+
+        if(lightChecker.LightLevel < minLightValue)
+        {
+            // Debug.Log("In Darkness: " + lightChecker.LightLevel);
+
+            if(darknessTimer == 0f)
+            {
+                darknessTimer = Time.time;
+            }
+
+            if((Time.time - darknessTimer) > maxDarknessTime)
+            {
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+        }
+        else
+        {
+            darknessTimer = 0f;
+        }
+	}
+
+    //call this to move the player 1 frame
+    private void Move()
+    {
+        float horizAxis = Input.GetAxis("Horizontal");
+        float vertAxis = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(movementSpeed * Time.deltaTime * horizAxis, rb.velocity.y, movementSpeed * Time.deltaTime * vertAxis);
+
+        rb.velocity = movement;
+        // transform.Translate(movement);
+    }
+}
