527f6209e22e92d14b09fd6fca77b75b5c1ac076
SUNDY-DESKTOP\nicks <nicksundermeyer@gmail.com>
2019-05-23 20:49:36 -0600

    Added new input system, changed pickup system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,153 +1,162 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
 
 public class PlayerController : MonoBehaviour {
     //movement variable
     public float movementSpeed = 20f;
 
-    // light level checking
-    public float minLightValue;
-    private LightCheck lightChecker;
-
-    // time allowed to be in darkness
-    private float darknessTimer = 0f;
-    public float maxDarknessTime;
-
+    // light detection variables
     public bool printLightLevel;
+    public float minLightValue;
+    public float maxDarknessTime;
+    private LightCheck lightChecker;
+    private float darknessTimer = 0f;
 
+    // GameObject references
     public GameObject Torch;
     private Torch torchComponent;
-
     public GameObject CampFire;
     private FireController campfireComponent;
-    
-    public int numSticks;
 
-    public bool isSwinging;
+    // editor variables
+    public int numSticks = 0;
+    public bool isSwinging = false;
+    public bool isDead = false;
 
+    // animation and audio
     private Animator anim;
     private AudioSource audio;
 
-    public bool isDead;
-
+    // component references
     private Rigidbody rb;
+    public GameObject pickupPos;
 
-    private void Awake() {
-        numSticks = 0;
-        isSwinging = false;
-        isDead = false;
+    // collision checking
+    private bool touchingCampfire = false;
+    private bool pickup = false; // whether we currently have something in our hand
+    private GameObject pickupObj = null;
 
-        lightChecker = GetComponentInChildren<LightCheck>();
-        rb = GetComponent<Rigidbody>();
-        campfireComponent = CampFire.GetComponent<FireController>();
-        torchComponent = Torch.GetComponent<Torch>();
-        anim = GetComponent<Animator>();
-        audio = GetComponent<AudioSource>();
+    private void Awake () {
+        lightChecker = GetComponentInChildren<LightCheck> ();
+        rb = GetComponent<Rigidbody> ();
+        campfireComponent = CampFire.GetComponent<FireController> ();
+        torchComponent = Torch.GetComponent<Torch> ();
+        anim = GetComponent<Animator> ();
+        audio = GetComponent<AudioSource> ();
     }
-	
-	// Update is called once per frame
-	void Update () {
-        Move();
 
-        if(printLightLevel)
-        {
-            Debug.Log("Light Level: " + lightChecker.LightLevel);
+    // Update is called once per frame
+    void Update () {
+        if (printLightLevel) {
+            Debug.Log ("Light Level: " + lightChecker.LightLevel);
         }
 
-        if(lightChecker.LightLevel < minLightValue)
-        {
+        if (lightChecker.LightLevel < minLightValue) {
             // Debug.Log("In Darkness: " + lightChecker.LightLevel);
 
-            if(darknessTimer == 0f)
-            {
+            if (darknessTimer == 0f) {
                 darknessTimer = Time.time;
             }
 
-            if((Time.time - darknessTimer) > maxDarknessTime)
-            {
-                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            if ((Time.time - darknessTimer) > maxDarknessTime) {
+                SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
             }
-        }
-        else
-        {
+        } else {
             darknessTimer = 0f;
         }
 
-	}
+    }
 
-    //call this to move the player 1 frame
-    private void Move()
-    {
-        float horizAxis = Input.GetAxis("Horizontal");
-        float vertAxis = Input.GetAxis("Vertical");
+    public void Move (InputAction.CallbackContext context) {
+        float horizAxis = context.ReadValue<Vector2> ().x;
+        float vertAxis = context.ReadValue<Vector2> ().y;
 
-        Vector3 movement = new Vector3(movementSpeed * Time.deltaTime * horizAxis, rb.velocity.y, movementSpeed * Time.deltaTime * vertAxis);
+        Vector3 movement = new Vector3 (movementSpeed * Time.deltaTime * horizAxis, rb.velocity.y, movementSpeed * Time.deltaTime * vertAxis);
 
         rb.velocity = movement;
     }
 
-    private void takeFuelForTorch(int amount)
-    {
-        if (campfireComponent.Fuel > amount)
-        {
+    public void Interact (InputAction.CallbackContext context) {
+        if (touchingCampfire) {
+            if (numSticks > 0)
+                putFuelInFire ();
+            else
+                takeFuelFromFire ();
+        }
+
+        // drop object if one is currently picked up, pick up if one is nearby
+        if (pickup) {
+            dropObject ();
+        } else if (pickupObj != null) {
+            pickupObject ();
+        }
+    }
+
+    private void pickupObject () {
+        // set kinematic and disable collider
+        pickupObj.GetComponent<Rigidbody> ().isKinematic = true;
+        pickupObj.GetComponent<Collider> ().enabled = false;
+        // parent object to pickup position
+        pickupObj.transform.parent = pickupPos.transform;
+        // set position and rotation
+        pickupObj.transform.localPosition = new Vector3 (0, 0, 0);
+        pickupObj.transform.localEulerAngles = new Vector3 (0, 0, 0);
+
+        pickup = true;
+    }
+
+    private void dropObject () {
+        // get currently picked up object
+        GameObject currentObj = pickupPos.transform.GetChild (0).gameObject;
+        // unparent it
+        currentObj.transform.parent = null;
+        // set non kinematic and enable collider
+        currentObj.GetComponent<Rigidbody> ().isKinematic = false;
+        currentObj.GetComponent<Collider> ().enabled = true;
+
+        pickup = false;
+    }
+
+    private void takeFuelFromFire () {
+        Debug.Log ("Just took 10 fuel");
+        int amount = 10;
+
+        if (campfireComponent.Fuel > amount) {
             campfireComponent.Fuel -= amount;
             torchComponent.torchFuel += amount;
-        }
-        else
-        {
-            Debug.Log("Not Enough Fuel!!");
+        } else {
+            Debug.Log ("Not Enough Fuel!!");
         }
     }
 
-    void takeFuelFromFire()
-    {
-        takeFuelForTorch(10);
-        Debug.Log("Just took 10 fuel");
+    private void putFuelInFire () {
+        Debug.Log ("Added " + (numSticks) + " to Fire");
+
+        campfireComponent.Fuel += numSticks;
+        numSticks = 0;
     }
 
-    private void OnCollisionStay(Collision col)
-    {
-        if(col.gameObject.name == "Campfire")
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {
-                campfireComponent.Fuel += numSticks ;
-                Debug.Log("Added " + (numSticks) + " to Fire");
-                //set sticks to 0
-                numSticks = 0;
-            }
-            else if (Input.GetKeyDown(KeyCode.E))
-            {
-                takeFuelFromFire();
-            }
-        }
-        else if(col.gameObject.name == "Enemy" || col.gameObject.name == "House")
-        {
+    // checking for collisions and setting variables
+    private void OnCollisionEnter (Collision other) {
+        // checking collisions with fire
+        touchingCampfire = (other.gameObject.name == "Campfire") ? true : false;
+
+        if (other.gameObject.name == "Enemy" || other.gameObject.name == "House")
             isDead = true;
-            //Debug.Log("you're dead!");
-        }
-
-        foreach(Transform child in transform)
-        {
-            if(child.name == "Torch")
-            {
-                if(Input.GetKeyDown(KeyCode.G))
-                {
-                    child.transform.parent = null;
-                }
-            }
-        }
-
     }
 
-    //function to pick up items
-    void OnControllColliderHit(ControllerColliderHit hitObj)
-    {
-        if(hitObj.gameObject.tag == "CanPickUp")
-        {
+    private void OnCollisionExit (Collision other) {
+        touchingCampfire = false;
+    }
 
-        }
+    private void OnTriggerEnter (Collider other) {
+        pickupObj = (other.gameObject.tag == "Pickup") ? other.gameObject : null;
+    }
+
+    private void OnTriggerExit (Collider other) {
+        pickupObj = null;
     }
 }
